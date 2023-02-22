0c2a0c24268c7553f0de3e44115a9fe5fb5785b9
JulianH09 <julianhiguita11@gmail.com>
2023-02-21 21:14:06 -0500

    first commit

---
+++ actividad2/java.js
@@ -0,0 +1,7 @@
+let comida = []
+
+for (let i = 0; i < 5; i++) {
+    let llamar = prompt("Ingrese comida")
+    comida.push(llamar)
+}
+console.log(comida)
---
+++ actividad3/java.js
@@ -0,0 +1,8 @@
+let numeros = [1, 4, 6, 10, 22, 55, 46, 2, 5, 0];
+
+for (let i = 0; i < 1; i++) {
+    numeros.sort(function(a, b) {
+        return a - b;
+      });
+    console.log(numeros)
+}
